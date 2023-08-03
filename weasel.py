28f4f7cc8320f79e3431cfae56169b0cd3ce1794
stphnmrgn <stephenmorganj@gmail.com>
2023-08-03 16:39:07 -0400

    refactor

--- weasel.py
+++ weasel.py
@@ -2,17 +2,32 @@
 weasel.py
 """
 import argparse
-from random import choice, choices, random
-from timer import Timer
 import string
+from random import choice, choices, random
+
+from timer import Timer
 
 
-def reproduce(phrase: list[str], p: float) -> list[str]:
+def _chars():
+    """
+    Return characters that are available for choices in random generation
+
+    Returns
+    -------
+    str
+        Character string
+    """
+    return string.ascii_uppercase + string.digits + string.punctuation + " "
+
+
+def _reproduce(chars: str, phrase: list[str], p: float) -> list[str]:
     """
     Return mutated list of characters
 
     Parameters
     ----------
+    chars : LiteralString
+        Available characters choices
     phrase : list[str]
         Characters that comprise a string
     p : float
@@ -23,64 +38,31 @@ def reproduce(phrase: list[str], p: float) -> list[str]:
     list[str]
         Mutated characters
     """
-    return [(choice(CHARS) if random() < p else char) for char in phrase]
-
-
-def fitness(phrase: list[str]) -> float:
-    """
-    The percent of letters in phrase that match and in the same position as
-    target
-
-    Parameters
-    ----------
-    phrase : list[str]
-        Characters that comprise a string
-
-    Returns
-    -------
-    float
-        Percent match, or fitness
-    """
-    fit = sum(t == h for t, h in zip(phrase, target))
-    return fit / len(phrase) * 100
+    return [(choice(chars) if random() < p else char) for char in phrase]
 
 
 @Timer()
-def main(target: list[str], p: float, c: int):
-    """
-    _summary_
+def main():
+    
+    def _fitness(phrase: list[str]) -> float:
+        """
+        The percent of letters in phrase that match and in the same position as
+        target
 
-    Parameters
-    ----------
-    target : list[str]
-        _description_
-    p : float
-        Mutation probability, by default 0.05
-    c : int
-        Number of children in each generation, by default 100
-    """
-    # generate random string with same length as TARGET
-    parent = choices(CHARS, k=len(target))
-    i = 0
-    print(
-        f"GEN: {i:03}, "
-        f"FITNESS: {fitness(parent):>3.0f}%, "
-        f"{''.join(str(char) for char in parent)}"
-    )
-    while parent != target:
-        i += 1
-        # Make (mutated) copies of the random string
-        progeny = (reproduce(parent, p) for _ in range(c))
-        # Compare each new copy with the target string, return highest scoring
-        parent = max(progeny, key=fitness)
-        print(
-            f"GEN: {i:03}, "
-            f"FITNESS: {fitness(parent):>3.0f}%, "
-            f"{''.join(str(char) for char in parent)}"
-        )
+        Parameters
+        ----------
+        phrase : list[str]
+            Characters that comprise a string
+
+        Returns
+        -------
+        float
+            Percent match, or fitness
+        """
+        fit = sum(t == h for t, h in zip(phrase, target))
+        return fit / len(phrase) * 100
 
 
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Weasel", formatter_class=argparse.MetavarTypeHelpFormatter
     )
@@ -105,8 +87,31 @@ if __name__ == "__main__":
         default=100,
         help="Number of children in each generation, by default 100",
     )
-    args = parser.parse_args()
 
-    CHARS = string.ascii_uppercase + string.digits + string.punctuation + " "
+    args = parser.parse_args()
     target = list(args.target.upper())
-    main(target, args.probability, args.children)
+    # characters available for random strings
+    CHARS = _chars()
+    # generate random string with same length as TARGET
+    parent = choices(CHARS, k=len(target))
+    i = 0
+    print(
+        f"GEN: {i:03}, "
+        f"FITNESS: {_fitness(parent):>3.0f}%, "
+        f"{''.join(str(char) for char in parent)}"
+    )
+    while parent != target:
+        i += 1
+        # Make (mutated) copies of the random string
+        progeny = (_reproduce(CHARS, parent, args.probability) for _ in range(args.children))
+        # Compare each new copy with the target string, return highest scoring
+        parent = max(progeny, key=_fitness)
+        print(
+            f"GEN: {i:03}, "
+            f"FITNESS: {_fitness(parent):>3.0f}%, "
+            f"{''.join(str(char) for char in parent)}"
+        )
+
+
+if __name__ == "__main__":
+    main()
